ec522dcdf47562922824b6ffd3de8b2e7581acd5
NarekPVP <hnarek2005@gmail.com>
2024-07-11 19:23:27 +0400

    🖼️ Added shadcn-ui article

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "http://localhost:3000",
+  url: "https://www.two-hundredth-ok.info",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
